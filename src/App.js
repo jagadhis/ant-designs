651df2ecf42e2e538ae2e0fcd8516aa7f4d790b0
jagadhis <jwaran78@gmail.com>
2022-02-09 12:11:33 +0530

    Created A Basic From

--- src/App.js
+++ src/App.js
@@ -1,22 +1,35 @@
 import logo from './logo.svg';
 import './App.css';
+import {Form,Button,Checkbox,DatePicker,Input,Select} from "antd";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Form>
+          <Form.Item name="Name" label="Name">
+            <Input placeholder="Enter Your Name"/>
+          </Form.Item>
+          <Form.Item name="DOB" label="DOB">
+            <Input placeholder="DD/MM/YYYY"/>
+          </Form.Item>
+          <Form.Item name="Email" label="Email">
+            <Input placeholder="Enter Your Email"/>
+          </Form.Item>
+          <Form.Item name="Gender" label="Gender">
+            <Select placeholder="Select Your Gender">
+            <Select.Option>Male</Select.Option>
+            <Select.Option>Female</Select.Option>
+            </Select>
+          </Form.Item>
+          <Form.Item name="Password" label="Password">
+            <Input.Password placeholder="Enter Password"/>
+          </Form.Item>
+          <Form.Item name="Confirm Password" label="Confirm Password">
+            <Input.Password placeholder="Re-enter Password"/>
+          </Form.Item>
+
+        </Form>
       </header>
     </div>
   );
